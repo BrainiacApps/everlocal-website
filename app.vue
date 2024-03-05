121d9c3684eb8210610367583d38c8463ba47334
elenaortega <elenaorfe@gmail.com>
2024-03-05 18:22:37 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div><NuxtPage /></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: "EverLocal",
+  ogTitle: "EverLocal",
+  description: "Be local anywhere.",
+  ogDescription: "Be local anywhere.",
+  twitterCard: "summary_large_image",
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="bg-emerald-500/50">
+    <section class="container mx-auto px-4">
+      <div class="flex items-center h-screen">
+        <div class="space-y-4">
+          <h1 class="text-xl lg:text-4xl">
+            Have you ever wanted to be local anywhere you go?
+          </h1>
+          <h2 class="lg:text-3xl text-gray-600">
+            If yes, then great, we are working in making it happen. Coming
+            soon...
+          </h2>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
